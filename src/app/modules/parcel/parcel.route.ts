0b7b6e0cd6d237c764a4d4485bda405113382e92
ajkn1904 <ajknishat@gmail.com>
2025-08-03 05:00:29 +0600

    parcel routes updated

--- src/app/modules/parcel/parcel.route.ts
+++ src/app/modules/parcel/parcel.route.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import { ParcelController } from "./parcel.controller";
 import { checkAuth } from "../../middlewares/checkAuth";
 import { Role } from "../user/user.interface";
+import { validationRequest } from "../../middlewares/validationRequest";
+import { createParcelZodSchema, updateParcelZodSchema } from "./parcel.validation";
 
 const router = Router(); 
 
-router.post("/create", checkAuth(Role.sender), ParcelController.createParcel);
+router.post("/create", validationRequest(createParcelZodSchema), checkAuth(Role.sender), ParcelController.createParcel);
 
 router.get("/", checkAuth(...Object.values(Role)), ParcelController.getAllParcels);
 
@@ -15,9 +17,9 @@ router.get("/:id", checkAuth(...Object.values(Role)), ParcelController.getSingle
 
 router.get("/history/:id", checkAuth(...Object.values(Role)), ParcelController.trackParcel);
 
-router.patch("/status/:id", checkAuth(Role.sender, Role.receiver), ParcelController.updateParcelStatus);
+router.patch("/status/:id", validationRequest(updateParcelZodSchema), checkAuth(Role.sender, Role.receiver), ParcelController.updateParcelStatus);
 
-router.patch("/admin/status/:id", checkAuth(Role.admin), ParcelController.updateParcelStatusByAdmin);
+router.patch("/admin/status/:id", validationRequest(updateParcelZodSchema), checkAuth(Role.admin), ParcelController.updateParcelStatusByAdmin);
 
 router.delete("/:id", checkAuth(Role.admin), ParcelController.deleteParcel);
 
