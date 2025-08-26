6bd8b451ceef31281a03fa826015db4ad01aac5c
ajkn1904 <ajknishat@gmail.com>
2025-08-27 04:31:42 +0600

    updated

--- src/app/utils/setCookie.ts
+++ src/app/utils/setCookie.ts
@@ -11,7 +11,7 @@ export const setAuthCookie = (res: Response, tokenInfo: AuthTokens) => {
     if(tokenInfo.accessToken){
         res.cookie("accessToken", tokenInfo.accessToken, {
             httpOnly: true,
-            secure: false,
+            secure: true,
             sameSite: "none"
         })
     }
@@ -19,7 +19,7 @@ export const setAuthCookie = (res: Response, tokenInfo: AuthTokens) => {
     if(tokenInfo.refreshToken){
         res.cookie("refreshToken", tokenInfo.refreshToken, {
             httpOnly: true,
-            secure: false,
+            secure: true,
             sameSite: "none"
         })
     }
